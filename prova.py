24642796e1e1c98d134a1ef272e27cb203d05158
xavi cosp <cospyx@gmail.com>
2015-11-19 08:52:41 +0100

    Implementación HOG

--- prova.py
+++ prova.py
@@ -3,16 +3,22 @@ import numpy as np
 from glob import glob
 import itertools as it
 import math as m
+from operator import add, truediv
 from numpy.linalg import norm
 import xml.etree.ElementTree as ET
 
-SZ = 8 # size of cell
-BSZ = 16 # size of block
+dimCell = 8 # size of cell
+dimBlock = 2 # size of block (2x2)Cells
+nbins = 9
+Err = 0.0000000001
+
 
 labels = []
-width = 0
-height = 0
+samples = []
 
+svm_params = dict( kernel_type = cv2.SVM_LINEAR,
+                    svm_type = cv2.SVM_C_SVC,
+                    C=2.67, gamma=5.383 )
 
 def split2d(img, cell_size, flatten=True):
     h, w = img.shape[:2]
@@ -22,6 +28,8 @@ def split2d(img, cell_size, flatten=True):
     if flatten:
         cells = cells.reshape(-1, sy, sx)
     return cells
+
+
 def load_image(fn):
 
     # Llegim imatge
@@ -39,58 +47,184 @@ def load_image(fn):
     #     return img
     # print img.shape
     return img
+
+
 def getCells(img):
-    cells = split2d(img, (SZ, SZ))
+    cells = split2d(img, (dimCell, dimCell))
     return cells
+
+
 def getGradients(img):
     height, width = img.shape
     # gradient_img = np.zeros((height,width))
     line_gradients = []
     gradient_img = []
-    for x in range(1,height-1):
-        for y in range(1, width-1):
+    counter = 0
+    for x in range(0, height):
+        for y in range(0, width):
+            if x == 0:
+                if y == 0 or y == width-1:
+                    px = 0.
+                    py = 0.
+                else:
+                    px = img[x,y+1] - img[x,y-1]
+                    py = 0.
+            elif x == height-1:
+                if  y == 0 or y == width-1:
+                    px = 0.
+                    py = 0.
+                else:
+                    px = img[x,y+1] - img[x,y-1]
+                    py = 0.
+            else:
+                if y == 0 or y == width-1:
+                    px = 0.
+                    py = img[x+1,y] - img[x-1,y]
+                else:
+                    px = img[x,y+1] - img[x,y-1]
+                    py = img[x+1,y] - img[x-1,y]
 
-            px = img[x+1,y] - img[x-1,y]
-            py = img[x,y+1] - img[x,y-1]
-
-            gradient = m.sqrt((px**2) + (py**2))
-            angle = np.arctan(py/px)
-            line_gradients.append([gradient, angle])
-        gradient_img.append(line_gradients)
-        line_gradients = []
+            magnitud = m.sqrt((px**2) + (py**2))
+            angle = m.degrees(getAngle(px,py))
+            gradient_img.append([magnitud, angle])
     return gradient_img
-def preprocess_hog(cells):
-    samples = []
 
-    for cell in cells:
-        print cell
-        # for x in range(0,SZ):
-        #     for y in range(0, SZ):
-        #
-        #         px = cell[x+1,y] - cell[x-1,y]
-        #         py = cell[x,y+1] - cell[x,y-1]
-        #
-        #         gradient = m.sqrt((px**2) + (py**2))
-        #         angle = np.arctan(py/px)
-        #         gradient_img.append([gradient,angle])
-    return np.float32(samples)
+
+def getAngle(x,y):
+    if  x == 0:
+        if y == 0:
+            angle = 0.
+        else:
+            angle = m.pi / 2
+    else:
+        if y == 0:
+            angle = 0.
+        else:
+            angle = np.arctan(y/x)
+    return angle
+
+
+def calculateHistogramCells(img, height, width):
+    nHorizCell = width / dimCell
+    nVerticalCell = height / dimCell
+    histogramCell = np.zeros(9)
+    histogramVector = []
+
+    for j in range(nVerticalCell):
+        for i in range(nHorizCell):
+            for y in range(j*dimCell, j*dimCell+dimCell):
+                for x in range(i*dimCell + y*width, i*dimCell + y*width + dimCell):
+                    magnitud, angle = img[x]
+
+                    if (90 >= angle > 70) or angle == -90:
+                        percentage1 = ( angle - 70 ) / 20.
+                        percentage2 = 1 - percentage1
+                        histogramCell[0] += magnitud * percentage1
+                        histogramCell[1] += magnitud * percentage2
+                    if 70 >= angle > 50:
+                        percentage1 = ( angle - 50 ) / 20.
+                        percentage2 = 1 - percentage1
+                        histogramCell[1] += magnitud * percentage1
+                        histogramCell[2] += magnitud * percentage2
+                    if 50 >= angle > 30:
+                        percentage1 = ( angle - 30 ) / 20.
+                        percentage2 = 1 - percentage1
+                        histogramCell[2] += magnitud * percentage1
+                        histogramCell[3] += magnitud * percentage2
+                    if 30 >= angle > 10:
+                        percentage1 = ( angle - 10 ) / 20.
+                        percentage2 = 1 - percentage1
+                        histogramCell[3] += magnitud * percentage1
+                        histogramCell[4] += magnitud * percentage2
+                    if 10 >= angle > -10:
+                        percentage1 = ( angle + 10 ) / 20.
+                        percentage2 = 1 - percentage1
+                        histogramCell[4] += magnitud * percentage1
+                        histogramCell[5] += magnitud * percentage2
+                    if -10 >= angle > -30:
+                        percentage1 = ( angle +30 ) / 20.
+                        percentage2 = 1 - percentage1
+                        histogramCell[5] += magnitud * percentage1
+                        histogramCell[6] += magnitud * percentage2
+                    if -30 >= angle > -50:
+                        percentage1 = ( angle + 50 ) / 20.
+                        percentage2 = 1 - percentage1
+                        histogramCell[6] += magnitud * percentage1
+                        histogramCell[7] += magnitud * percentage2
+                    if -50 >= angle > -70:
+                        percentage1 = ( angle + 70 ) / 20.
+                        percentage2 = 1 - percentage1
+                        histogramCell[7] += magnitud * percentage1
+                        histogramCell[8] += magnitud * percentage2
+                    if -70 >= angle > -90:
+                        percentage1 = ( angle + 90 ) / 20.
+                        percentage2 = 1 - percentage1
+                        histogramCell[8] += magnitud * percentage1
+                        histogramCell[0] += magnitud * percentage2
+                    # print percentage1, percentage2
+            histogramVector.append(histogramCell)
+            histogramCell = np.zeros(9)
+    return histogramVector, nVerticalCell, nHorizCell
+
+
+def normalizeHistogramsBlocks(vector_hist,width,height):
+
+    dimVert = height - 1
+    dimHoriz = width - 1
+    resto_j = 0
+    resto_i = 0
+    blockHistogramVector = []
+    sumHistogram = [0.,0.,0.,0.,0.,0.,0.,0.,0.]
+    histogramsNormalized = []
+
+    for j in range(dimVert):
+        resto_i = 0
+        if j != 0:
+            resto_j += 1
+        for i in range(dimHoriz):
+            if i != 0:
+                resto_i += 1
+            for y in range(j*dimBlock, j*dimBlock + dimBlock):
+                control_y = y
+                control_y -= resto_j
+                for x in range(i*dimBlock + control_y*width, i*dimBlock + control_y*width + dimBlock ):
+                    index = x
+                    index -= resto_i
+                    blockHistogramVector.append(vector_hist[index])
+                    sumHistogram = map(add, sumHistogram, vector_hist[index])
+            for z in range(len(blockHistogramVector)):
+                for w in range(nbins):
+                    if sumHistogram[w] != 0:
+                        histogramsNormalized.append(blockHistogramVector[z][w] / sumHistogram[w])
+                    else:
+                        histogramsNormalized.append(0.)
+
+            # histogramsNormalized += blockHistogramVector
+            # histogramsNormalized = np.append(histogramsNormalized, blockHistogramVector)
+            blockHistogramVector = []
+            sumHistogram = [0.,0.,0.,0.,0.,0.,0.,0.,0.]
+    # histogramsNormalized = np.float32(histogramsNormalized)
+    # histogramsNormalized = np.hstack(histogramsNormalized)
+    return histogramsNormalized
+
 
 hog = cv2.HOGDescriptor()
 svm = cv2.SVM()
 
-train_pos = glob('train/pos/*')
-train_neg = glob('train/neg/*')
-test_pos = glob('test/pos/*')
-test_neg = glob('test/neg/*')
-test_mit = glob('MIT/*')
-test_prova = glob('prova/*')
+# train_pos = glob('train/pos/*')
+# train_neg = glob('train/neg/*')
+# test_pos = glob('test/pos/*')
+# test_neg = glob('test/neg/*')
+# test_mit = glob('MIT/*')
+test_prova_pos = glob('prova/aux/*')
+test_prova_neg = glob('prova/neg/*')
 
-for fn in it.chain(test_prova):
+for fn in it.chain(test_prova_pos):
     try:
         # Retornem imatge en escala de grisos normalitzada
         img = load_image(fn)
         height, width = img.shape
-        # print img
+
         if img is None:
             print 'Failed to load image file:', fn
             continue
@@ -100,15 +234,42 @@ for fn in it.chain(test_prova):
         print 'loading error'
         continue
     # Calculem el gradient de la imatge
-    gradient_img = getGradients(img)
+    gradient_img = getGradients(img) # Mirar la funcio cartToPolar
+    # Calculem histograma per cells de Ndimensions
+    histogram_vector, nVertCell, nHorizCell = calculateHistogramCells(gradient_img, height, width)
+    # Normalitzem per blocks
+    histogram_vector_normalized = normalizeHistogramsBlocks(histogram_vector, nHorizCell, nVertCell)
+    # histogram_vector_normalized = np.float32(histogram_vector_normalized)
+    samples.append(histogram_vector_normalized)
 
-    # cells = split2d(gradient_img, (SZ, SZ))
+for fn in it.chain(test_prova_neg):
+    try:
+        # Retornem imatge en escala de grisos normalitzada
+        img = load_image(fn)
+        img = cv2.resize(img, (128, 64))
 
-    # Separem imatges en cells
-    # cells = getCells(img)
-    # # Agafem
-    # samples = preprocess_hog(cells)
+        height, width = img.shape
 
+        if img is None:
+            print 'Failed to load image file:', fn
+            continue
+        else:
+            labels.append([0.])
+    except:
+        print 'loading error'
+        continue
+    # Calculem el gradient de la imatge
+    gradient_img = getGradients(img) # Mirar la funcio cartToPolar
+    # print gradient_img
+    # Calculem histograma per cells de Ndimensions
+    histogram_vector, nVertCell, nHorizCell = calculateHistogramCells(gradient_img, height, width)
+    # Normalitzem per blocks
+    histogram_vector_normalized = normalizeHistogramsBlocks(histogram_vector, nHorizCell, nVertCell)
+    # histogram_vector_normalized = np.float32(histogram_vector_normalized)
+    samples.append(histogram_vector_normalized)
 
+samples = np.float32(samples)
+labels = np.float32(labels)
 
+svm.train(samples, labels, params=svm_params)
 
